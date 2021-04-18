2cbe66e096771e17aa28fc6483e186e5c53ca150
Saransh <saransh0701@gmail.com>
2021-04-18 22:11:19 +0530

    Add NoteDatabase and NoteRepository

--- app/src/main/java/com/example/todo/NoteDAO.kt
+++ app/src/main/java/com/example/todo/NoteDAO.kt
@@ -1,14 +1,15 @@
 package com.example.todo
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 
 @Dao
 interface NoteDAO {
     @Query("SELECT * FROM notes_table ORDER BY id ASC")
-    fun getAllNotes(): List<Note>
+    fun getAllNotes(): LiveData<List<Note>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insert(note: Note)
+    suspend fun insert(note: Note)
 
     @Delete
     suspend fun delete(note: Note)
